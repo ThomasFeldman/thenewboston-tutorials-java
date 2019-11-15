6252fb5a50761de01d78ccd9438896840fdd79de
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-15 05:29:08 -0600

    IDE changed to eclipse, first video tutorial done

--- Youtube.java
+++ Youtube.java
@@ -1,5 +1,5 @@
-class apples {
-    public static void main(String args[]){
-        System.out.println("Hello Youtube");
+class Youtube{
+    public static void main(String args[]) {
+    	System.out.println("Hello Youtube");
     }
 }
