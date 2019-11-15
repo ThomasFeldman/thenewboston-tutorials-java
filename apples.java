f96b721a8bd69d43f6627e84c2f416f92a8b9d56
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-15 05:54:16 -0600

    Basic Calculator

--- apples.java
+++ apples.java
@@ -1,7 +1,13 @@
 import java.util.Scanner;
-class apples {
+class apples{
 	public static void main(String args[]) {
 		Scanner bucky = new Scanner(System.in);
-		System.out.println(bucky.nextLine());
+		double fnum, snum, answer;
+		System.out.println("Enter first num: ");
+		fnum = bucky.nextDouble();
+		System.out.println("Enter second num: ");
+		snum = bucky.nextDouble();
+		answer = fnum + snum;
+		System.out.println("Answer equals: " + answer);
 	}
 }
