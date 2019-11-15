cd8dcd884eff8339213e505b95398b47f41e112e
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-15 06:13:54 -0600

    Math Operators

--- apples.java
+++ apples.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 class apples{
 	public static void main(String args[]) {
 		Scanner bucky = new Scanner(System.in);
-		double fnum, snum, answer;
-		System.out.println("Enter first num: ");
-		fnum = bucky.nextDouble();
-		System.out.println("Enter second num: ");
-		snum = bucky.nextDouble();
-		answer = fnum + snum;
-		System.out.println("Answer equals: " + answer);
+		int girls, boys, people;
+		girls = 11;
+		boys = 3;
+		people = girls/boys;
+		
+		System.out.println(people);
 	}
 }
