3bffd4039cf3141529e39f43f63b04dbb8e40ee4
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-16 16:41:57 -0600

    Using Multiple Classes

--- apples.java
+++ apples.java
@@ -1,10 +1,6 @@
 class apples{
 	public static void main(String args[]) {
-		int counter = 0;
-		
-		while(counter < 10) {
-			System.out.println(counter);
-			counter++;
-		}
+		tuna tunaObject = new tuna();
+		tunaObject.simpleMessage();
 	}
 }
