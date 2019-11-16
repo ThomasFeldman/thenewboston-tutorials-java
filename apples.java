3aba73bdf547abfd19b1755f2e600eaadb413fb7
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-16 16:12:39 -0600

    Switch Statement

--- apples.java
+++ apples.java
@@ -1,13 +1,21 @@
 class apples{
 	public static void main(String args[]) {
-		int boy, girl;
-		boy = 8;
-		girl = 99;
+		int age;
+		age = 6;
 		
-		if (boy > 10 || girl < 60) {
-			System.out.println("You can enter");
-		}else {
-			System.out.println("You can not enter");
+		switch (age) {
+		case 1:
+			System.out.println("You can crawl");
+			break;
+		case 2:
+			System.out.println("You can talk");
+			break;
+		case 3,4,5,6,7:
+			System.out.println("You can get in trouble");
+			break;
+		default:
+			System.out.println("I don't know how old you are");
+			break;
 		}
 	}
 }
