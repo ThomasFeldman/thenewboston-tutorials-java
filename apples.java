1b5b9478d3000eb470948a9f21ee8d72ac32427f
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-16 16:25:09 -0600

    While Loops

--- apples.java
+++ apples.java
@@ -1,21 +1,10 @@
 class apples{
 	public static void main(String args[]) {
-		int age;
-		age = 6;
+		int counter = 0;
 		
-		switch (age) {
-		case 1:
-			System.out.println("You can crawl");
-			break;
-		case 2:
-			System.out.println("You can talk");
-			break;
-		case 3,4,5,6,7:
-			System.out.println("You can get in trouble");
-			break;
-		default:
-			System.out.println("I don't know how old you are");
-			break;
+		while(counter < 10) {
+			System.out.println(counter);
+			counter++;
 		}
 	}
 }
