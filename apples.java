5c1b9be5ae6eea5532e476c5af178a37fd2740f1
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-18 06:04:03 -0600

    Conditional Operators

--- apples.java
+++ apples.java
@@ -1,14 +1,7 @@
 class apples{
 	public static void main(String args[]) {
-		int age = 45;
-		if(age > 60) {
-			System.out.println("You are a senior citizen");
-		}else if(age >=50) {
-			System.out.println("You are in your 50's");
-		}else if(age >=40) {
-			System.out.println("You are in your 40's");
-		}else {
-			System.out.println("You are a young buck");
-		}
+		int age = 21;
+		
+		System.out.println(age > 50 ? "You are old": "You are young");
 	}
 }
