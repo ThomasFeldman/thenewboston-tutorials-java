bbd7532b4e8898e2a9a2c93f17b20a6d8c5ecb66
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-18 05:50:59 -0600

    Nested If Statements

--- apples.java
+++ apples.java
@@ -1,11 +1,16 @@
-import java.util.Scanner;
 class apples{
 	public static void main(String args[]) {
-		tuna tunaObject = new tuna("Kelsey");
-		tuna tunaObject2 = new tuna("Nicole");
-		tunaObject.saying();
-		System.out.println("");
-		tunaObject2.saying();
-
+		int age = 60;
+		
+		if (age < 50) {
+			System.out.println("You are young");
+		}else {
+			System.out.println("You are old");
+			if (age > 75) {
+				System.out.println("You are REALLY old!");
+			}else {
+				System.out.println("dont worry you arent really that old");
+			}
+		}
 	}
 }
