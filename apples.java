0ae7890ca713bf5653e415fa7af74ec9fe4b28ca
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-19 05:12:05 -0600

    Math Class Methods

--- apples.java
+++ apples.java
@@ -1,10 +1,5 @@
 class apples{
 	public static void main(String args[]) {
-		int counter = 15;
-		
-		do {
-			System.out.println(counter);
-			counter++;
-		}while(counter <= 10);
+		System.out.println(Math.sqrt(9));
 	}
 }
