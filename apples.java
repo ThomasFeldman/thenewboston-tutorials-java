3b855cef5f179ffa31578b265d6e5c3bb957a999
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-19 05:36:48 -0600

    Introduction to Arrays

--- apples.java
+++ apples.java
@@ -1,12 +1,8 @@
-import java.util.Random;
 class apples{
 	public static void main(String args[]) {
-		Random dice = new Random();
-		int number;
 		
-		for (int counter=1; counter <=10; counter++) {
-			number = 1 + dice.nextInt(6);
-			System.out.println(number + " ");
-		}
+		int bucky[]= {2,4,5,7,9};
+		
+		System.out.println(bucky[2]);
 	}
 }
