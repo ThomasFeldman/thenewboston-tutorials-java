6d69959415b37a5d646e25467e4113c635265e14
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-19 05:55:05 -0600

    Creating an Array Table

--- apples.java
+++ apples.java
@@ -1,8 +1,10 @@
 class apples{
 	public static void main(String args[]) {
+		System.out.println("Index\tValue");
+		int bucky[] = {32,12,18,54,2};
 		
-		int bucky[]= {2,4,5,7,9};
-		
-		System.out.println(bucky[2]);
+		for(int counter = 0; counter<bucky.length; counter++) {
+			System.out.println(counter + "\t" + bucky[counter]);
+		}
 	}
 }
