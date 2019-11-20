6d96408b4644a751076239396560dcd5925ee421
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-19 20:53:00 -0600

    Table for Multi Arrays

--- apples.java
+++ apples.java
@@ -1,16 +1,20 @@
 class apples{
 	public static void main(String args[]) {
-		int bucky[] = {1,2,3,4,5};
-		change(bucky);
-		
-		for(int y: bucky) {
-			System.out.println(y);
-		}
+		int firstarray[][]= {{8,9,10,11}, {12,13,14,15}};
+		int secondarray[][]= {{30,31,32,33}, {43}, {4,5,6}};
+		System.out.println("This is the first array");
+		display(firstarray);
+		System.out.println("This is the second array");
+		display(secondarray);
+	
 	}
 	
-	public static void change(int x[]) {
-		for(int counter=0; counter<x.length;counter++) {
-			x[counter] += 5;
+	public static void display(int x[][]) {
+		for(int row = 0; row<x.length;row++) {
+			for(int column = 0; column<x[row].length; column++) {
+				System.out.print(x[row][column] + "\t");
+			}
+			System.out.println();
 		}
-	}
+	} 
 }
