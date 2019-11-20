a8985ee30d08f24a59aeff149e1b9433e0fee428
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-20 06:47:53 -0600

    Variable Length Arguments

--- apples.java
+++ apples.java
@@ -1,20 +1,15 @@
 class apples{
 	public static void main(String args[]) {
-		int firstarray[][]= {{8,9,10,11}, {12,13,14,15}};
-		int secondarray[][]= {{30,31,32,33}, {43}, {4,5,6}};
-		System.out.println("This is the first array");
-		display(firstarray);
-		System.out.println("This is the second array");
-		display(secondarray);
-	
+		System.out.print(average(43,56,76,8,65,76,2,31));
 	}
 	
-	public static void display(int x[][]) {
-		for(int row = 0; row<x.length;row++) {
-			for(int column = 0; column<x[row].length; column++) {
-				System.out.print(x[row][column] + "\t");
-			}
-			System.out.println();
+	public static int average(int...numbers) {
+		int total = 0;
+		
+		for(int x: numbers) {
+			total+=x;
 		}
-	} 
+		
+		return total/numbers.length;
+	}
 }
