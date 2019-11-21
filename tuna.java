df363c7a20c54addc97346944538f731dd0fa144
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-21 06:40:08 -0600

    Public, Private, and This

--- tuna.java
+++ tuna.java
@@ -1,10 +1,12 @@
 public class tuna {
-    private int hour, minute, second;
+    private int hour = 1;
+    private int minute = 2;
+    private int second = 3;
     
-    public void setTime(int h, int m, int s){
-    hour = ((h>=0 && h<24) ? h : 0);
-    minute = ((m>=0 && m<60) ? m:0);
-    second = ((s>=0 && s<60) ? s : 0);
+    public void setTime(int hour, int minute, int second){
+    	this.hour = 4;
+    	this.minute = 5;
+    	this.second = 6;
   }
 
     public String toMilitary(){
