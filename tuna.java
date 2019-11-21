f05dd73d560e2d1ff092955e584a6208cc856508
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-21 06:54:04 -0600

    Multiple Constructors

--- tuna.java
+++ tuna.java
@@ -1,18 +1,24 @@
 public class tuna {
-    private int hour = 1;
-    private int minute = 2;
-    private int second = 3;
-    
-    public void setTime(int hour, int minute, int second){
-    	this.hour = 4;
-    	this.minute = 5;
-    	this.second = 6;
-  }
-
-    public String toMilitary(){
-    	return String.format("%02d:%02d:%02d", hour, minute, second);
-  }
-    public String toString(){
-    	return String.format("%d:%02d:%02d %s", ((hour==0 || hour==12)?12:hour) %12, minute, second, (hour < 12 ? "AM" : "PM"));
-    }
+	private int hour;
+	private int minute;
+	private int second;
+	
+	public tuna() {
+		this(0,0,0);
+	}
+	public tuna(int h) {
+		this(h,0,0);
+	}
+	public tuna(int h, int m) {
+		this(h,m,0);
+	}
+	public tuna(int h, int m, int s) {
+		setTime(h,m,s);
+	}
+	public void setTime(int h, int m, int s) {
+		setHour(h);
+		setMinute(m);
+		setSecond(s);
+	}
+	
 }
