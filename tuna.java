30df8f39fa9470b8a7d5fe9a34b03e9804cc4c7a
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-22 16:44:03 -0600

    Set and Get Methods

--- tuna.java
+++ tuna.java
@@ -20,5 +20,25 @@ public class tuna {
 		setMinute(m);
 		setSecond(s);
 	}
-	
+	public void setHour(int h) {
+		hour = ((h>=0&&h<24)?h:0);
+	}
+	public void setMinute(int m) {
+		minute = ((m>=0&&m<60)?m:0);
+	}
+	public void setSecond(int s) {
+		second = ((s>=0&&s<60)?h:0);
+	}
+	public int getHour() {
+		return hour;
+	}
+	public int getMinute() {
+		return minute;
+	}
+	public int getSecond() {
+		return second;
+	}
+	public String toMilitary() {
+		return String.format("%02d:%02d:%02d", getHour(),getMinute(),getSecond())
+	}
 }
