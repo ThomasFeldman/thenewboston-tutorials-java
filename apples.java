cc8618f449b099101b3e33a9bf3470bec9a4b1d8
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-25 05:12:07 -0600

    Inheritance

--- apples.java
+++ apples.java
@@ -1,12 +1,6 @@
-import java.util.EnumSet;
 class apples{
     public static void main(String args[]){
-    	tuna tunaObject = new tuna(10);
-    	for(int i = 0; i<5; i++) {
-    		tunaObject.add();
-    		System.out.printf("%s", tunaObject);
-    	}
-    	
-    }
-    
+    	tuna tunaObject = new tuna(); 	
+    	tunaObject.eat();
+    }   
 }
--- tuna.java
+++ tuna.java
@@ -1,14 +1,3 @@
-public class tuna {
-	private int sum;
-	private final int NUMBER;
+public class tuna extends food {
 	
-	public tuna(int x) {
-		NUMBER = x;
-	}
-	public void add() {
-		sum += NUMBER;
-	}
-	public String toString() {
-		return String.format("sum = %d\n", sum);
-	}
 }
