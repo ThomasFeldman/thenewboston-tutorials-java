eea0728b5826f509492b472358ae78f2790475c3
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-25 05:28:41 -0600

    Graphical User Interface GUI

--- apples.java
+++ apples.java
@@ -1,6 +1,14 @@
+import javax.swing.JOptionPane;
+
 class apples{
     public static void main(String args[]){
-    	tuna tunaObject = new tuna(); 	
-    	tunaObject.eat();
+    	String fn = JOptionPane.showInputDialog("Enter first number");
+    	String sn = JOptionPane.showInputDialog("Enter second number");
+
+    	int num1 = Integer.parseInt(fn);
+    	int num2 = Integer.parseInt(sn);
+    	int sum = num1 + num2;
+    	
+    	JOptionPane.showMessageDialog(null, "The answer is " + sum, "the title", JOptionPane.PLAIN_MESSAGE);
     }   
 }
