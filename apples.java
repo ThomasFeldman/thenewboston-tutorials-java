6f0d33fbec979696a7d3883b1453ad8198eecb85
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-24 19:25:02 -0600

    Enumeration

--- apples.java
+++ apples.java
@@ -1,10 +1,8 @@
 class apples{
     public static void main(String args[]){
-    
-    	potpie potObject = new potpie(4,5,6);
-    	
-    	tuna tunaObject = new tuna("Bucky", potObject);
-    	System.out.println(tunaObject);
+    	for(tuna people: tuna.values()) {
+    		System.out.printf("%s\t%s\t%s\n", people, people.getDesc(), people.getYear());
+    	}
     }
     
 }
