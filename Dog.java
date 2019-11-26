bdcd699411b3528cf14c6c468b836317c6c3d399
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-26 06:33:41 -0600

    Array Holding Many Objects

--- Dog.java
+++ Dog.java
@@ -1,4 +1,3 @@
-
 public class Dog extends Animal{
 
 }
--- apples.java
+++ apples.java
@@ -1,9 +1,11 @@
 class apples{
     public static void main(String args[]){
-
-    	DogList DLO = new DogList();
-    	Dog d = new Dog();
-    	DLO.add(d);
     	
+    	AnimalList ALO = new AnimalList();
+    	Dog d = new Dog();
+    	Fish f = new Fish();
+    	
+    	ALO.add(d);
+    	ALO.add(f);
     }
 }
