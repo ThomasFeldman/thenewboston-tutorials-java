741c6c4212abb74aba9c73d60bfc2179fc2a5f8e
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-27 20:36:09 -0600

    JButton Final Program

--- apples.java
+++ apples.java
@@ -1,5 +1,9 @@
+import javax.swing.JFrame;
 class apples{
     public static void main(String args[]){
-    	
+    	Gui go = new Gui();
+    	go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	go.setSize(300, 200);
+    	go.setVisible(true);
     }
 }
