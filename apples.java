24a96519cb36a7d4b1f86fc3d0de7d11d9447c62
ThomasFeldman <tfeldman1.31@gmail.com>
2019-12-03 06:35:10 -0600

    File Class

--- apples.java
+++ apples.java
@@ -1,10 +1,12 @@
-import javax.swing.JFrame;
+import java.io.File;;
 class apples{
-    public static void main(String args[]){
-    	
-    	Gui go = new Gui();
-    	go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	go.setSize(300, 200);
-    	go.setVisible(true);
+	public static void main(String args[]) {
+		File x = new File("C:\\test\\fhkdjsf.txt");
+		
+		if (x.exists()) {
+			System.out.println(x.getName() + "exist!");
+		}else {
+			System.out.println("this thing doestnt eisists");
+		}
     }
 }
