cbd0bb557b7ffef7a983a503bb6ff4fbafef7b6d
ThomasFeldman <tfeldman1.31@gmail.com>
2019-12-03 06:56:43 -0600

    Writing to Files

--- apples.java
+++ apples.java
@@ -3,15 +3,9 @@ import java.util.*;
 class apples{
 	public static void main(String args[]) {
 		
-		final Formatter x;
-		
-		try {
-			x = new Formatter("fred.text");
-			System.out.println("you crested a file");
-		}
-		catch(Exception e) {
-			System.out.println("you got an error");
-		}
-		
+		createfile g = new createfile();
+		g.openFile();
+		g.addRecords();
+		g.closeFile();
     }
 }
