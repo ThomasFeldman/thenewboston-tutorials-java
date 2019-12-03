fb91c0ec4c89760f6be33619db9ad0c1d8336d75
ThomasFeldman <tfeldman1.31@gmail.com>
2019-12-03 06:26:55 -0600

    Adapter Classes

--- Gui.java
+++ Gui.java
@@ -3,48 +3,27 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Gui extends JFrame{
-	private JPanel mousepanel;
+	private String details;
 	private JLabel statusbar;
-	
 	public Gui() {
-		super("title");
-		
-		mousepanel = new JPanel();
-		mousepanel.setBackground(Color.WHITE);
-		add(mousepanel, BorderLayout.CENTER);
-		
-		statusbar = new JLabel("default");
+		super("tutle");
+		statusbar = new JLabel("this is default");
 		add(statusbar, BorderLayout.SOUTH);
+		addMouseListener(new Mouseclass());
 		
-		Handlerclass handler = new Handlerclass();
-		mousepanel.addMouseListener(handler);
-		mousepanel.addMouseMotionListener(handler);
 	}
-	
-	private class Handlerclass implements MouseListener, MouseMotionListener{
+	private class Mouseclass extends MouseAdapter{
 		public void mouseClicked(MouseEvent event) {
-			statusbar.setText(String.format("Clicked at %d,%d", event.getX(), event.getY()));
-		}
-		public void mousePressed(MouseEvent event) {
-			statusbar.setText("you pressed down the mouse");
-		}
-		public void mouseReleased(MouseEvent event) {
-			statusbar.setText("you released the button");
-		}
-		public void mouseEntered(MouseEvent event) {
-			statusbar.setText("y0ou entered the area");
-			mousepanel.setBackground(Color.RED);
-		}
-		public void mouseExited(MouseEvent event) {
-			statusbar.setText("the mouse hasd left he window");
-			mousepanel.setBackground(Color.WHITE);
-		}
-		// these are mouse motion events
-		public void mouseDragged(MouseEvent event) {
-			statusbar.setText("you are dragging the mouse");
-		}
-		public void mouseMoved(MouseEvent event){
-			statusbar.setText("you moved the mouse");
+			details = String.format("You clicked %d", event.getClickCount());
+
+			if(event.isMetaDown()) {
+				details += "with right mouse button";
+			}else if(event.isAltDown()) {
+				details += "with center moust button";
+			}else {
+				details += "wiuth left mouse button";
+			}
+			statusbar.setText(details);
 		}
 	}
 }
