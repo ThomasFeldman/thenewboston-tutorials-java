4144b8db88ddb578740b593fe57e9ec00e24b61c
ThomasFeldman <tfeldman1.31@gmail.com>
2019-12-04 08:40:47 -0600

    Reading from Files

--- apples.java
+++ apples.java
@@ -3,9 +3,9 @@ import java.util.*;
 class apples{
 	public static void main(String args[]) {
 		
-		createfile g = new createfile();
-		g.openFile();
-		g.addRecords();
-		g.closeFile();
+		readfile r = new readfile();
+		r.openFile();
+		r.readFile();
+		r.closeFile();
     }
 }
