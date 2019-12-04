dd900ea7d1ca6574b9a06db99e41bf749539d5c2
ThomasFeldman <tfeldman1.31@gmail.com>
2019-12-04 09:35:29 -0600

    Drawing Graphics

--- bucky.java
+++ bucky.java
@@ -2,9 +2,11 @@ import javax.swing.*;
 
 public class bucky {
 	public static void main(String args[]) {
-		Layout l = new Layout();
-		l.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		l.setSize(300,100);
-		l.setVisible(true);
+		JFrame f = new JFrame("Title");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Peach p = new Peach();
+		f.add(p);
+		f.setSize(400,250);
+		f.setVisible(true);
 	}
 }
