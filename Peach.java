596bc487acb7d062a13d6d7c6e0e81281640d625
ThomasFeldman <tfeldman1.31@gmail.com>
2019-12-05 06:56:28 -0600

    Drawing More Stuff

--- Peach.java
+++ Peach.java
@@ -2,32 +2,22 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Peach extends JFrame{
+public class Peach extends JPanel{
 	
-	private JButton b;
-	private Color color = (Color.WHITE);
-	private JPanel panel;
-	
-	public Peach() {
-		super("the title");
-		panel = new JPanel();
-		panel.setBackground(color);
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		this.setBackground(Color.WHITE);
 		
-		b = new JButton("Choose a color");
-		b.addActionListener(
-			new ActionListener() {
-				public void actionPerformed(ActionEvent event) {
-					color = JColorChooser.showDialog(null, "Pick your color", color);
-					if(color==null) {
-						color=(Color.WHITE);
-					}
-					panel.setBackground(color);
-				}
-			}	
-		);
-		add(panel, BorderLayout.CENTER);
-		add(b, BorderLayout.SOUTH);
-		setSize(425,150);
-		setVisible(true);
+		g.setColor(Color.BLUE);
+		g.drawLine(10, 25, 200, 45);
+		
+		g.setColor(Color.RED);
+		g.drawRect(10, 55, 100, 30);
+		
+		g.setColor(Color.GREEN);
+		g.fillOval(10, 95, 100, 30);
+		
+		g.setColor(Color.ORANGE);
+		g.fill3DRect(10, 160, 100, 50, true);
 	}
 }
--- bucky.java
+++ bucky.java
@@ -1,10 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class bucky {
 	public static void main(String args[]) {
 		
+		JFrame f = new JFrame("Title");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		Peach p = new Peach();
-		p.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		p.setBackground(Color.WHITE);
+		f.add(p);
+		f.setSize(500, 270);
+		f.setVisible(true);
 		
 	}
 }
