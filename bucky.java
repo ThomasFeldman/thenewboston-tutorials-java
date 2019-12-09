e824a24f0fa85804a66feaf602d06db6aa636e40
ThomasFeldman <tfeldman1.31@gmail.com>
2019-12-08 19:19:05 -0600

    Introduction to Collections

--- bucky.java
+++ bucky.java
@@ -1,14 +1,22 @@
+import java.util.*;
 public class bucky {
 	public static void main(String args[]) {
-		System.out.println(fact(5));
-	}
-	
-	//fact
-	public static long fact(long n) {
-		if(n<= 1) {
-			return 1;
-		}else {
-			return n * fact(n-1);
+		String[] things = {"eggs", "lasers", "hats", "pies"};
+		List<String> list1 = new ArrayList<String>();
+		
+		//add array items to list
+		for(String x: things) {
+			list1.add(x);
+			}
+		String[] morethings = {"lasers", "hats"};
+		List<String> list2 = new ArrayList<String>();
+
+		//add array items to list
+		for(String y: morethings) {
+			list2.add(y);
+			}
+		for(int i = 0; i<list2.size(); i++) {
+			System.out.printf("%s ", list2.get(i));
 		}
 	}
 }
