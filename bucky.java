845ddc62327ba2eca7ca78128bb9e4b6086d35a1
ThomasFeldman <tfeldman1.31@gmail.com>
2019-12-09 21:36:47 -0600

    LinkedList

--- bucky.java
+++ bucky.java
@@ -1,43 +1,27 @@
 import java.util.*;
 public class bucky {
 	public static void main(String args[]) {
-		String[] things = {"eggs ", "lasers ", "hats ", "pie"};
-		List<String> list1 = new ArrayList<String>();
-		
-		//add array items to list
+	
+		String[] things = {"apples", "noobs", "pwnge", "bacon", "goATS"};
+		List<String> list1 = new LinkedList<String>();
 		for(String x: things) {
 			list1.add(x);
-			}
+		}
 		
-		String[] morethings = {"lasers", "hats"};
-		List<String> list2 = new ArrayList<String>();
-
-		//add array items to list
-		for(String y: morethings) {
+		String[] things2 = {"sausage", "bacon", "goats", "harrypotter"};
+		List<String> list2 = new LinkedList<String>();
+		for(String y: things2) {
 			list2.add(y);
-			}
-		
-		//print out list 1
-		for(int i = 0; i<list1.size(); i++) {
-			System.out.printf("%s", list1.get(i));
 		}
 		
-		editlist(list1,list2);
-		System.out.println();
+		list1.addAll(list2);
+		list2 = null;
 		
-		//print out new list 1 
-		for(int i = 0; i<list1.size(); i++) {
-			System.out.printf("%s", list1.get(i));
-		}
+		printMe(list1);
+		removeStuff(list1, 2,5);
+		printMe(list1);
+		reverseMe(list1);
 		
-	}
-	
-	public static void editlist(Collection<String> l1, Collection<String> l2) {
-		Iterator<String> it = l1.iterator();
-		while(it.hasNext()) {
-			if(l2.contains(it.next())) {
-				it.remove();
-			}
-		}
+		
 	}
 }
