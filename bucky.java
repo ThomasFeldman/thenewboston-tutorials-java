438e0dd85b378d5e7332cca4b3ec6cc0a76a9733
ThomasFeldman <tfeldman1.31@gmail.com>
2019-12-10 21:32:05 -0600

    Converting Lists to Arrays

--- bucky.java
+++ bucky.java
@@ -1,46 +1,19 @@
 import java.util.*;
 public class bucky {
 	public static void main(String args[]) {
-	
-		String[] things = {"apples", "noobs", "pwnge", "bacon", "goATS"};
-		List<String> list1 = new LinkedList<String>();
-		for(String x: things) {
-			list1.add(x);
+		
+		String[] stuff = {"babies", "watermelong", "melons", "fudge"};
+		LinkedList<String> thelist = new LinkedList<String>(Arrays.asList(stuff));
+		
+		thelist.add("pumpikinf");
+		thelist.addFirst("firstthing");
+		
+		//cponmvert back to an array
+		stuff = thelist.toArray(new String[thelist.size()]);
+		
+		for(String x: stuff) {
+			System.out.printf("%s ", x);
 		}
 		
-		String[] things2 = {"sausage", "bacon", "goats", "harrypotter"};
-		List<String> list2 = new LinkedList<String>();
-		for(String y: things2) {
-			list2.add(y);
-		}
-		
-		list1.addAll(list2);
-		list2 = null;
-		
-		printMe(list1);
-		removeStuff(list1, 2,5);
-		printMe(list1);
-		reverseMe(list1);		
-	}
-	
-	//printMe method
-	private static void printMe(List<String> l) {
-		for(String b: l) {
-			System.out.printf("%s ", b);
-		}
-		System.out.println();
-	}
-	
-	//removeStuff method
-	private static void removeStuff(List<String> l, int from, int to) {
-		l.subList(from, to).clear();
-	}
-	
-	//reverseMe
-	private static void reverseMe(List<String> l) {
-		ListIterator<String> bobby = l.listIterator(l.size());
-		while(bobby.hasPrevious()) {
-			System.out.printf("%s ", bobby.previous());
-		}
 	}
 }
