0b79d263c81f83e3a6dd3c70dedd501afe7c251b
ThomasFeldman <tfeldman1.31@gmail.com>
2019-12-10 21:24:22 -0600

    LinkedList Program

--- bucky.java
+++ bucky.java
@@ -20,8 +20,27 @@ public class bucky {
 		printMe(list1);
 		removeStuff(list1, 2,5);
 		printMe(list1);
-		reverseMe(list1);
-		
-		
+		reverseMe(list1);		
+	}
+	
+	//printMe method
+	private static void printMe(List<String> l) {
+		for(String b: l) {
+			System.out.printf("%s ", b);
+		}
+		System.out.println();
+	}
+	
+	//removeStuff method
+	private static void removeStuff(List<String> l, int from, int to) {
+		l.subList(from, to).clear();
+	}
+	
+	//reverseMe
+	private static void reverseMe(List<String> l) {
+		ListIterator<String> bobby = l.listIterator(l.size());
+		while(bobby.hasPrevious()) {
+			System.out.printf("%s ", bobby.previous());
+		}
 	}
 }
