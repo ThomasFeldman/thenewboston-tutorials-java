9b5cbd9d006b97e2c92e4cb42ca909852eba57dd
ThomasFeldman <tfeldman1.31@gmail.com>
2019-12-11 19:10:28 -0600

    addAll

--- bucky.java
+++ bucky.java
@@ -1,39 +1,25 @@
 import java.util.*;
 public class bucky {
 	public static void main(String args[]) {
-		
-		//create an array and convert to lsit
-		Character[] ray = {'p', 'w', 'n'};
-		List<Character> l = Arrays.asList(ray);
-		System.out.println("List is : ");
-		output(l);
-		
-		
-		//reverse and print out the lsit
-		Collections.reverse(l);
-		System.out.println("After reverse : ");
-		output(l);
-		
-		//createa new array and a new lsit
-		Character[] newRay = new Character[3];
-		List<Character> listCopy = Arrays.asList(newRay);
-		
-		//copy contents of list into lsitcopy
-		Collections.copy(listCopy, l);
-		System.out.println("Copy of list : ");
-		output(listCopy);
-		
-		//fill collection with crap
-		Collections.fill(l, 'X');
-		System.out.println("After filling the lsit : ");
-		output(l);
-	}
-	private static void output(List<Character> thelist) {
-		
-		for(Character thing: thelist) {
-			System.out.printf("%s ", thing);
-		}
-		System.out.println();
-	}
 	
+		//convert stuff array to alist
+		String[] stuff = {"apples", "beef", "corn", "ham"};
+		List<String> list1 = Arrays.asList(stuff);
+		
+		ArrayList<String> list2 = new ArrayList<String>();
+		list2.add("youtube");
+		list2.add("google");
+		list2.add("digg");
+		
+		for(String x: list2) {
+			System.out.printf("%s ", x);
+		}
+		
+		Collections.addAll(list2, stuff);
+		
+		System.out.println();
+		for(String x: list2) {
+			System.out.printf("%s ", x);
+		}
+	}
 }
