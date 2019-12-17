16022e997e5ff54a4a223fd23e5c806e624b5dfc
ThomasFeldman <tfeldman1.31@gmail.com>
2019-12-16 20:44:19 -0600

    Introduction to Applets (DEPRECATED)

--- bucky.java
+++ bucky.java
@@ -1,23 +1,9 @@
-import java.util.*;
-public class bucky {
-	public static void main(String args[]) {
-	
-		System.out.println(max(23, 42, 1));
-		System.out.println(max("apples", "tots", "chicken"));
-		
-	}
-	
-	public static <T extends Comparable<T>> T max(T a, T b, T c) {
-		T m = a;
-		
-		if(b.compareTo(a) > 0) {
-			m = b;
-		}
-		
-		if(c.compareTo(m) > 0) {
-			m = c;
-		}
-		
-		return m;
+import java.awt.*;
+import javax.swing.*;
+
+public class bucky extends JApplet{
+	public void paint(Graphics g) {
+		super.paint(g);
+		g.drawString("wow this actually worked?", 25, 25);
 	}
 }
