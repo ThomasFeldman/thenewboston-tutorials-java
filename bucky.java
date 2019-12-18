d8887f18f8666b8c019a9c7fd186596c7bffd5d8
ThomasFeldman <tfeldman1.31@gmail.com>
2019-12-17 21:22:08 -0600

    init for Applets (DEPRECATED)

--- bucky.java
+++ bucky.java
@@ -2,9 +2,22 @@ import java.awt.*;
 import javax.swing.*;
 
 public class bucky extends JApplet{
+	
+	private double sum;
+	
+	public void init() {
+		String fn = JOptionPane.showInputDialog("Enter first number");
+		String sn = JOptionPane.showInputDialog("Enter second number");
+		
+		double n1 = Double.parseDouble(fn);
+		double n2 = Double.parseDouble(sn);
+		
+		sum = n1 + n2;
+	}
+	
 	public void paint(Graphics g) {
 		super.paint(g);
-		g.drawString("wow this actually worked?", 25, 25);
+		g.drawString("The sum is "+sum, 25, 30);
 	}
 	
 }
