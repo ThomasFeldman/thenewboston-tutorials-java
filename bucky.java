bc8f6a0a2342b6cf1b509c95a780dfc517dbcf32
ThomasFeldman <tfeldman1.31@gmail.com>
2019-12-20 19:30:20 -0600

    Learning about Threads

--- bucky.java
+++ bucky.java
@@ -1,11 +1,6 @@
-import java.awt.*;
-import javax.swing.*;
 
 public class bucky {
 	public static void main(String[] args) {
-		TheWindow w = new TheWindow();
-		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		w.setSize(230, 280);
-		w.setVisible(true);
+		
 	}
 }
--- tuna.java
+++ tuna.java
@@ -1,5 +1,20 @@
-public class tuna extends food {
-	void eat() {
-		System.out.println("this tuna is great");
+import java.util.*;
+
+public class tuna implements Runnable {
+	String name;
+	int time;
+	Random r = new Random();
+	
+	public tuna(String x) {
+		name = x;
+		time = r.nextInt(999);
+	}
+	
+	public void run() {
+		try {
+			System.out.printf("%s is sleeping for %d", name, time);
+			Thread.sleep(time);
+			System.out.printf("%s is done\n", name);
+		}catch(Exception e) {}
 	}
 }
