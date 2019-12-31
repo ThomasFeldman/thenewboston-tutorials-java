fc160529729fa98c78f3bbeadaed0c34156201ba
ThomasFeldman <tfeldman1.31@gmail.com>
2019-12-30 19:41:05 -0600

    loadCrap

---
+++ ReadFileMain.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+
+public class ReadFileMain {
+	public static void Main(String[] args) {
+		ReadFile dude = new ReadFile();
+		dude.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
